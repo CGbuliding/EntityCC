9ce455d14e6a0267ba5cfd3dc61de89533fcf55e
Akanyi <tong1.10@foxmail.com>
2024-07-04 01:43:14 +0800

    update

--- src/main/java/link/sakacloud/entitycc/EntityCC.java
+++ src/main/java/link/sakacloud/entitycc/EntityCC.java
@@ -1,11 +1,9 @@
 package link.sakacloud.entitycc;
 
-import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Silverfish;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -25,30 +23,20 @@ public class EntityCC extends JavaPlugin {
 
     private class SilverfishListener implements Listener {
 
-        private int silverfishCount = 0;
-
-
-        @EventHandler
-        public void onEntityDeath(EntityDeathEvent event) {
-
-            if (event.getEntity() instanceof Silverfish) {
-                silverfishCount--;
-            }
-        }
-
         @EventHandler
         public void onEntitySpawn(EntitySpawnEvent event) {
             if (event.getEntity() instanceof Silverfish) {
-                silverfishCount++;
+                Collection<? extends Silverfish> silverfishes = event.getEntity().getWorld().getEntitiesByClass(Silverfish.class);
 
-                if (silverfishCount > maxSilverfish) {
-                    World world = event.getEntity().getWorld();
-                    List<Silverfish> silverfishes = (List<Silverfish>) world.getEntitiesByClass(Silverfish.class);
-                    silverfishes.sort(Comparator.comparingLong(Entity::getTicksLived));
-                    silverfishes.getFirst().remove();
-                    silverfishCount--;
-                }
+
+                    while (silverfishes.size() < maxSilverfish) {
+                        silverfishes.stream()
+                                .min(Comparator.comparingLong(Entity::getTicksLived))
+                                .ifPresent(Entity::remove);
+                    }
+
             }
         }
     }
 }
+
